6bb60f02b656cd5ee18a3ecfdb009c9fb4db1389
Musab Salah <musabsalah1998@gmail.com>
2023-02-24 14:58:12 +0200

    Add project files.

---
+++ Student County/BusinessLogic/Helpers/Common/TrackableData.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_County.BusinessLogic.Helpers.Common
+{
+    public class TrackableData
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
